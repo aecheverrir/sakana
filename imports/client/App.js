699ee3c3b58b6f848abb3f72cab248778a9e0850
s-cortes <s.cortes@uniandes.edu.co>
2018-03-31 20:28:57 -0500

    nuevos componentes de npm y meteor para usuarios y routers

--- imports/client/App.js
+++ imports/client/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Banner from './Banner';
 import Hero from './Hero';
 import Menu from './Menu';
 import Footer from './Footer' 
 
-export default class App extends React.Component{
+export default class App extends Component{
     constructor(){
         super();
         this.state = {
