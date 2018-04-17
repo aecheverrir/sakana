cdf30771707c96eb20936a6d6da85bdccb9349f5
s-cortes <s.cortes@uniandes.edu.co>
2018-04-16 22:19:06 -0500

    Se debe agregar otra linea en el after para restaurar la variable Meteor.userId()

--- imports/api/pedidos.tests.js
+++ imports/api/pedidos.tests.js
@@ -5,20 +5,24 @@ import { Pedidos } from './pedidos.jsx';
 import { assert } from 'meteor/practicalmeteor:chai';
 import { sinon } from 'meteor/practicalmeteor:sinon';
 import { resetDatabase } from "meteor/xolvio:cleaner";
+import { fail } from 'assert';
 
-const newUser = {
-  email: "email@fake.com",
-  password: "PasswordOne",
-  username: "UsernameOne",
-  profile: {
-    name: "Name One",
-    phoneNumber: "phoneNumber"
-  }
-};
-let userIdentification;
+
+
 
 if (Meteor.isServer) {
   describe("pedidos", function ( ) {
+
+    let userIdentification;
+    const newUser = {
+      email: "email@fake.com",
+      password: "PasswordOne",
+      username: "UsernameOne",
+      profile: {
+        name: "Name One",
+        phoneNumber: "phoneNumber"
+      }
+    };
 
     beforeEach(function ( ) {
       resetDatabase();
@@ -29,6 +33,11 @@ if (Meteor.isServer) {
       sinon.stub(Meteor, "userId");
       Meteor.userId.returns(userIdentification);
 
+    });
+
+    afterEach(function ( ) {
+      resetDatabase();
+      sinon.restore(Meteor, "userId");
     });
 
     describe("pedidos.crud",function ( ) {
@@ -60,7 +69,39 @@ if (Meteor.isServer) {
         let pedidoPersistido = Pedidos.findOne(pedido);
         console.log(pedidoPersistido);
         assert(pedidoPersistido);
-      })
+      });
+
+      it("should not insert a new Element", function () {
+
+        let pedido = {
+          owner: (userIdentification + "AA"),
+          address: "calle prueba 123",
+          creationDate: new Date(),
+          comments: "Comentario de prueba",
+          price: 100,
+          items: [
+            {
+              name: "Item Pruba Uno",
+              description: "Descipcion item prueba Uno",
+              price: 50
+            },
+            {
+              name: "Item Pruba Dos",
+              description: "Descipcion item prueba Dos",
+              price: 50
+            }
+          ]
+        };
+        try {
+          Meteor.call("pedidos.insert", pedido);
+          fail(1,1,"Deberia generar error");
+        } catch (error) {
+          assert(true);
+        }
+        
+        
+      });
+
     })
 
   });
